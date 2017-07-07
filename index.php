dc6d6629ce223a0475aaa832c0d6d35898764eb6
Jim Cummins <jimthedev@gmail.com>
2017-07-06 20:32:07 -0500

    The final commit. Miss you Doug.

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
 
             /* Full screen */
             @media only screen and (min-width : 790px) {
-                #left { margin-right: 350px; }
+                
                 #right { width: 300px; height: 450px; position: absolute; top:0; right:0; }
                 .bubble {
                     background: #f0ede5;
@@ -240,184 +240,16 @@
     <body>
         <div id="container">
             <div id="header"><img src="/img/mileslogo.jpg?cachebust=20150312-01"><hr></div>
-            <div id="content">
-                <div id="right">
-                    <div id="action-buttons" class="bubble with-margin-bottom">
-                        <!-- <div class="tshirtnote">Note: T-shirt orders are no longer available.</div> -->
-                        <!-- <a href="https://www.raceentry.com/races/eastern-iowa-miles-for-myeloma/2016/register" class="button">Register to run/walk!</a> -->
-                        <a href="#" class="button button-disabled button-smaller">Registration coming soon!</a>
-                        <!-- <div class="small-text"><br><strong>PLEASE NOTE-</strong> T-SHIRTS WILL NOT BE AVAILABLE FOR REGISTRATIONS MADE AFTER MIDNIGHT SEPTEMBER 25.<br><br>
-                        * CONTINUE TO REGISTER EITHER AS <strong>INDIVIDUALS (No T-Shirt)</strong> or <strong>TEAM (No T-Shirt)</strong>.<br></div> -->
-                        <span class="action-button-separator"><br><br> - OR -<br><br></span>
-                        <a href="http://support.themmrf.org/goto/MilesforMM2016" class="button">Donate to the cause.</a>
-                        <div id="more-info" class="mini-section with-top-padding">
-                            <!-- <hr>
-                            <div id="new-this-year">
-                                <h3>New this year</h3>
-                                <p>
-                                    <ul>
-                                        <li>awards</li>
-                                        <li>team sponsorship</li>
-                                    </ul>
-                                    More information on these fun new options is coming soon!</span>
-                                </p>
-                            </div>
-                        -->
-                            <hr>
-                        </div>
-                        <div id="faq-teaser" class="mini-section">
-                            <div id="new-this-year">
-                                <h3>Team Registration</h3>
-                                <p>
-                                    <ul>
-                                        <li><a href="#faq-create-team">Create A Team</a></li>
-                                        <li><a href="#faq-join-team">Join A Team</a></li>
-                                    </ul>
-                                </p>
-                            </div>
-                            <hr>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div id="left">
-                <h3>About us</h3>
-                <p>Eastern Iowa Miles for Myeloma is excited to announce the <span class="accent">6th Annual Miles for Myeloma Run/Walk</span> on <span class="accent">Sunday, October 1, 2017</span> at <span class="accent">Terry Trueblood Recreation Area</span> in Iowa City, Iowa.</p>
-                <p>Email us any time for more information at <a href="mailto:info@milesformm.com">info@milesformm.com</a>.
-                <h3>Schedule</h3>
-                <ul>
-                    <li>10 am &#8211; Same Day Registration and T-shirt pickup, coffee and snacks</li>
-                    <li>11 am &#8211; Run/Walk (2 or 4 mile)</li>
-                </ul>
-                <h3>Location</h3>
-                <p class="location-map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47706.83878154421!2d-91.54388431387528!3d41.641103491203765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87e4402e2e9fb6df%3A0xe3c8206f23db1cb7!2sTerry+Trueblood+Recreation+Area!5e0!3m2!1sen!2sus!4v1401848578452" frameborder="0" style="border:0"></iframe></p>
-                <h3>Add to your calendar</h3>
-                <p class="add-to-calendar">
-                    <!-- Snippet -->
-                    <div class="addthisevent">
-                        <span class="_start">10/01/2017 10:00:00 AM</span>
-                        <span class="_end">10/01/2017 01:00:00 PM</span>
-                        <span class="_zonecode">11</span>
-                        <span class="_summary">A 2 or 4 mile run/walk benefiting Multiple Myeloma research.</span>
-                        <span class="_description">Miles for Myeloma is an annual 2 or 4 miles run/walk that raises funds and promotes awareness for Multiple Myeloma. It is organized by Eastern Iowa Miles for Myeloma.</span>
-                        <span class="_location">Terry Trueblood Recreation Area 4213 Sand Rd SE Iowa City, IA 52240 United States</span>
-                        <span class="_organizer">Eastern Iowa Miles for Myeloma</span>
-                        <span class="_organizer_email">info@milesformm.com</span>
-                        <span class="_all_day_event">false</span>
-                        <span class="_date_format">MM/DD/YYYY</span>
-                    </div>
-                </p>
-            </div>
-            <div id="help-collect" class="add-mobile-space">
-                <h3>Help us collect donations</h3>
-                <p>We have forms to help you gather donations on behalf of Miles for Myeloma. Check out our <a href="#forms" class="accent">forms</a> section for more information.</p>
-            </div>
-            <hr>
-            <div id="car-donation" class="add-mobile-space">
-                <a name="car-donation"></a>
-                <h3>Car Donation to Eastern Iowa Miles for Myeloma</h3>
-                <p>Donate your car, boat, truck, RV, Jet Ski or snowmobile to Eastern Iowa Miles for Myeloma and receive a tax deduction. This no cost, no hassle process begins when you contact our fundraising partner, Donation Line LLC at 877-227-7487. Make sure to ask for our extension 3101. Or you can click here <a href="http://www.donationline.com/newvehicle_donation_form.shtml?n=Iowa%20City%20Miles%20for%20Myeloma" class="accent">"Donate Car"</a> to donate online now.</p>
-            </div>
-            <hr>
-            <div id="employer-matching" class="add-mobile-space">
-                <a name="employer-matching"></a>
-                <h3>Matching Gifts</h3>
-                <p>Many companies offer a Matching Gifts program, whereby employers match contributions made by employees. This is a BENEFIT for the employees. Be sure to use it! </p>
-                <p>Here is all you have to do:<p>
-                <ol>
-                    <li>The employee fills out the Matching Gift form from their employer it will either be an online form or can be obtained through the employee's HR department</li>
-                    <li>After completing the form, either forward the confirmation to <a href="mailto:events@themmrf.org">events@themmrf.org</a> or if it is a form that needs to be processed by the MMRF, fax it to (203) 229-0572.  MMRF will handle from there.</li>
-                    <li>To have the match credited to Eastern Iowa Miles For Myeloma please send an email to <a href="mailto:info@milesformm.com">info@milesformm.com</a>
-                </ol>
-            </div>
-            <hr>
-            <div id="faq" class="add-mobile-space">
-                <h3>Frequently Asked Questions (FAQ)</h3>
-                <div id="questions">
-                    <div class="question">
-                        <a name="faq-create-team"></a>
-                        <h4>Create A Team</h4>
-                        <div class="question-content">
-                            <h5>First, Establish your team to become a team captain, then decide on t-shirts</h5>
-                            <ul>
-                                <li>You need a team name and an 8 character team password.</li>
-                                <li>You need to decide if you will use the Miles for Myeloma event t-shirts, if you will create your own team t-shirt or if you will let individuals wear what they would like.</li>
-                            </ul>
-                            <h5>Second, Create your team on the run/walk registration website.</h5>
-                            <ol>
-                                <!-- <li>Go to the Eastern Iowa Miles for Myeloma <a href="https://www.raceentry.com/races/eastern-iowa-miles-for-myeloma/2016/register" class="accent">Registration Page</a></li> -->
-                                <li>Once registration opens, go to the Eastern Iowa Miles for Myeloma <span class="text-disabled">Registration Page (registration coming soon)</span></li>
-                                <li>Go to <strong>Entry Type</strong> - Select "Team".</li>
-                                <li>Go to <strong>Team Entry</strong> - Select "Create Team" and select the type of team (with or without t-shirt).</li>
-                                <li>Go to <strong>Team Name</strong> - Enter the name of your team.</li>
-                                <li>Go to <strong>Team Password</strong> - Enter your 8 character password.</li>
-                                <li>Go to <strong>Verify Password</strong> - Enter your 8 character password.</li>
-                                <li>Complete the Team Captain registration information(name, address, email address, etc.).</li>
-                                <li>After completing all required fields - hit "Continue"</li>
-                                <li>Complete the Payment Page and hit "Register".</li>
-                                <li>A confirmation email will be sent to the Team Captain's email address.</li>
-                            </ol>
-                            <h5>Third, Instruct team members to join your team.</h5>
-                            <ul>
-                                <li>Send the team name and password to people who you'd like to join your team.</li>
-                                <li>Send the <a href="#faq-join-team" class="accent">'join a team'</a> instructions to people who you'd like to join your team.</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="question">
-                        <a name="faq-join-team"></a>
-                        <h4>Join A Team</h4>
-                        <div class="question-content">
-                            <h5>First, get your registration information for your team captain</h5>
-                            <ul>
-                                <li>Get your team name from your team captain.</li>
-                                <li>Get your team password from your team captain.</li>
-                            </ul>
-                            <h5>Second, register for the run/walk using the information from your team captain</h5>
-                            <ol>
-                                <!-- <li>Go to the Eastern Iowa Miles for Myeloma <a href="https://www.raceentry.com/races/eastern-iowa-miles-for-myeloma/2016/register" class="accent">Registration Page</a></li> -->
-                                <li>Once registration opens, go to the Eastern Iowa Miles for Myeloma <span class="text-disabled">Registration Page (registration coming soon)</span></li>
-                                <li>Go to <strong>Entry Type</strong> - Select "Team"</li>
-                                <li>Go to <strong>Team Entry</strong> - Select the type of team (with or without t-shirt) you will have.</li>
-                                <li>Go to <strong>Team Name</strong> - Select your team from the drop down box.</li>
-                                <li>Go to <strong>Team Password</strong> - Enter the team password.</li>
-                                <li>Complete the individual team member registration information (name, address, email address, etc.).</li>
-                                <li>After completing all required fields - hit "Continue"</li>
-                                <li>Complete the Payment Page and hit "Register".</li>
-                                <li>A confirmation email will be sent to the team member's email address.</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr>
-            <div id="forms" class="add-mobile-space">
-                <a name="forms"></a>
-                <h3>Forms and Other Information</h3>
-                <ul>
-                    <li>
-                        <a href="/milesformyeloma.2017.pledge.form.pdf">Pledge Form</a>
-                    </li>
-                <!--
-                    <li>
-                        <a href="/donationthankyouletter.pdf">Donation Thank You Letter</a>
-                    </li>
-                    <li>
-                        <a href="/milesformyeloma.2015.single.sided.flyer.pdf">Run/Walk Flyer</a>
-                    </li>
-                -->
-                    <li>
-                        <a href="/milesformyeloma.2016.single.sided.flyer.pdf">Sponsor Flyer</a>
-                    </li>
-                    <!--<li>
-                        Sponsor flyer (Coming soon)
-                    </li>-->
-                <!--
-                    <li>
-                        <a href="/milesformyeloma.2015.trifold.brochure.pdf">Trifold Informational Brochure</a>
-                    </li>
-                -->
-                </ul>
+              <h3>Dear EIMM Friends,</h3>
+              <p>After long, serious thought, we regretfully announce that there will be no EIMM Walk/Run this October.  After five incredible years of working with all of you, it's time to give our attention to many other things we hold dear.  We continue to support the Multiple Myeloma Research Foundation and firmly believe in its mission.</p>
+
+              <p>Join us September 24 for the MMRF 5K Walk/Run in Minneapolis <a href="https://walkrun.themmrf.org/twin-cities">https://walkrun.themmrf.org/twin-cities</a>, and donate to the foundation: <a href="https://www.themmrf.org/donate-to-mmrf/">https://www.themmrf.org/donate-to-mmrf/</a>.</p>
+
+              <p>Effusive thanks to the walkers, runners, volunteers, team members, sponsors, and families (including our own) for support beyond what we ever dreamed possible.  We wish you health and love. </p>
+
+              <p>Dan and Laurie</p>
+
             </div>
             <div id="footer">
                     <hr>
